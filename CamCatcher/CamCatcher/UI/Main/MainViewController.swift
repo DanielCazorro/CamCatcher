14b0eefc0579315b6b5eba08d104c0662687d7e9
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-26 11:39:24 +0100

    Create tbbAddButton actions: Select form galery, take picture and cancel

--- CamCatcher/CamCatcher/UI/Main/MainViewController.swift
+++ CamCatcher/CamCatcher/UI/Main/MainViewController.swift
@@ -15,14 +15,13 @@ class MainViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var tbMainToolbar: UIToolbar!
     @IBOutlet weak var tbbAddButton: UIBarButtonItem!
+    @IBOutlet weak var tbMainTableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         configureAddButton()
-}
-
+    }
 
     // MARK: - Methods
     
@@ -33,7 +32,31 @@ class MainViewController: UIViewController {
     
     func configureAddButton() {
         // TODO: Center the button
-
+        
+        // Agregar acción al botón de añadir
+        tbbAddButton.target = self
+        tbbAddButton.action = #selector(addButtonTapped)
+    }
+    
+    @objc func addButtonTapped() {
+        // Mostrar el pop-up para elegir entre la galería y la cámara
+        let alertController = UIAlertController(title: "Añadir imagen a la tabla", message: "Elija una opción", preferredStyle: .actionSheet)
+        
+        let galleryAction = UIAlertAction(title: "Seleccionar de la galería", style: .default) { _ in
+            // Implementar la lógica para seleccionar de la galería
+        }
+        
+        let cameraAction = UIAlertAction(title: "Tomar foto", style: .default) { _ in
+            // Implementar la lógica para tomar una foto
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        alertController.addAction(galleryAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 
 
