35694fec0f90d96aaa5e7511b7e18b2f476ed0b5
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-26 14:05:17 +0100

    Pop Up working. Now we make another screen

--- CamCatcher/CamCatcher/UI/Main/MainViewController.swift
+++ CamCatcher/CamCatcher/UI/Main/MainViewController.swift
@@ -69,6 +69,40 @@ extension MainViewController: UIImagePickerControllerDelegate, UINavigationContr
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    /// Método para mostrar el pop-up o modal con la imagen en grande
+    private func showImagePopup(image: UIImage) {
+        // Crear un controlador de alerta
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        // Crear un UIImageView para mostrar la imagen en el pop-up
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit // Ajusta el modo de contenido al tamaño de la imagen
+        alertController.view.addSubview(imageView) // Agrega el UIImageView al pop-up
+
+        
+        // Configurar las restricciones para el UIImageView dentro del pop-up
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: alertController.view.topAnchor, constant: 80).isActive = true // Establece la posición superior
+        imageView.bottomAnchor.constraint(equalTo: alertController.view.bottomAnchor, constant: -80).isActive = true // Establece la posición inferior
+        imageView.leadingAnchor.constraint(equalTo: alertController.view.leadingAnchor, constant: 80).isActive = true // Establece la posición izquierda
+        imageView.trailingAnchor.constraint(equalTo: alertController.view.trailingAnchor, constant: -80).isActive = true // Establece la posición derecha
+        imageView.widthAnchor.constraint(equalToConstant: 400).isActive = true // Establece el ancho fijo
+        imageView.heightAnchor.constraint(equalToConstant: 400).isActive = true // Establece la altura fija
+        imageView.centerXAnchor.constraint(equalTo: alertController.view.centerXAnchor).isActive = true // Centra horizontalmente
+        imageView.centerYAnchor.constraint(equalTo: alertController.view.centerYAnchor).isActive = true // Centra verticalmente
+        imageView.layer.cornerRadius = 10 // Establece el radio de la esquina
+
+
+        
+        // Agregar un botón de "Cerrar" al pop-up
+        let closeAction = UIAlertAction(title: "Cerrar", style: .default, handler: nil)
+        alertController.addAction(closeAction)
+        
+        // Presentar el controlador de alerta
+        present(alertController, animated: true, completion: nil)
+    }
+
 }
 
 extension MainViewController: UITableViewDataSource {
@@ -78,7 +112,7 @@ extension MainViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        160 // Altura de cada fila de la tabla (en puntos)
+        104 // Altura de cada fila de la tabla (en puntos)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -96,5 +130,14 @@ extension MainViewController: UITableViewDataSource {
         }
         
         return cell // Devuelve la celda configurada para mostrar una imagen
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let image = viewModel?.images[indexPath.row] {
+            // Mostrar el pop-up con la imagen en grande
+            showImagePopup(image: image)
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
