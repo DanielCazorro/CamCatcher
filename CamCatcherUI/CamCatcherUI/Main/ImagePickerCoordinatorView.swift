793876cb26c7fcd6379c3f39863405e9618c251e
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-27 12:38:29 +0100

    Add comments to ImagePickerCoordinatorView

--- CamCatcherUI/CamCatcherUI/Main/ImagePickerCoordinatorView.swift
+++ CamCatcherUI/CamCatcherUI/Main/ImagePickerCoordinatorView.swift
@@ -11,12 +11,15 @@ struct ImagePickerCoordinatorView: UIViewControllerRepresentable {
     typealias UIViewControllerType = UIImagePickerController
     var imagePickerCoordinator: ImagePickerCoordinator
     
+    /// Esta función crea y devuelve un UIImagePickerController
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
+        // Asigna el coordinador como el delegado del UIImagePickerController
         picker.delegate = imagePickerCoordinator
-        picker.sourceType = imagePickerCoordinator.sourceType 
+        // Configura el sourceType del UIImagePickerController con el sourceType del coordinador
+        picker.sourceType = imagePickerCoordinator.sourceType
         return picker
     }
-    
+    /// Esta función no hace nada porque no necesitamos actualizar la vista del controlador
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
 }
