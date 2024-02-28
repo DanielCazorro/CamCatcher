7a9340f672017c0db0b2880167e980e9d2fa34ab
Daniel Cazorro <leinadhunter10@hotmail.com>
2024-02-28 10:15:18 +0100

    Add zoom in- zoom out

--- CamCatcherUI/CamCatcherUI/Detail/DetailView.swift
+++ CamCatcherUI/CamCatcherUI/Detail/DetailView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    @State private var zoomValue = 1.0
+    
     var image: UIImage
     
     var body: some View {
-        // Muestra la imagen en una vista redimensionable y con aspecto de ajuste
-        Image(uiImage: image)
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .edgesIgnoringSafeArea(.all) // Ignora los márgenes seguros para que la imagen ocupe toda la pantalla
-            .navigationBarTitle("", displayMode: .inline) // Configura el título de la barra de navegación
+        ZoomableScrollView {
+            // Muestra la imagen en una vista redimensionable y con aspecto de ajuste
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .ignoresSafeArea()
+                .scaleEffect(zoomValue)
+                .accessibilityZoomAction { action in
+                    let zoomQuantity = "\(Int(zoomValue)) x zoom"
+                    switch action.direction {
+                    case .zoomIn:
+                        zoomValue += 1.0
+                        AccessibilityNotification.Announcement(zoomQuantity).post()
+                    case .zoomOut:
+                        zoomValue -= 1.0
+                        AccessibilityNotification.Announcement(zoomQuantity).post()
+                    }
+                }
+            //            .resizable()
+            //            .aspectRatio(contentMode: .fit)
+            //            .edgesIgnoringSafeArea(.all) // Ignora los márgenes seguros para que la imagen ocupe toda la pantalla
+            //            .navigationBarTitle("", displayMode: .inline) // Configura el título de la barra de navegación
+        }
     }
 }
 
